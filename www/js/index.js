e6ab06e1865542958b4670f12cf08c57f48e67cc
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-04-30 07:02:59 +0300

    .

---
+++ www/js/index.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  this.document.addEventListener('deviceready', init, false);
+  init();
+}
+
+function init() {
+  document.getElementById('btnAdd').addEventListener('click', displayList, false);
+  document.getElementById('btnClear').addEventListener('click', clearList, false);
+}
+
+function addSong(){
+
+}
+
+function displaList() {
+
+}
+
+function clearList() {
+  
+}
